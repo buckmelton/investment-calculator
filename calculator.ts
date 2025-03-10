e974540e07844426106f9e45ad2a6377f07bc2f7
Buck Melton <buckm@alumni.stanford.edu>
2025-03-10 08:51:22 -0700

    Add initial code for investment calculator

---
+++ calculator.ts
@@ -0,0 +1,15 @@
+// data:
+// initial investment
+// annual contribution
+// expected return
+// duration
+
+function calculateInvestment(data) {} // => result[]
+
+function printResults(results) {
+  // print (output) the results
+}
+
+const results = calculateInvestment(data);
+
+printResults(results);
